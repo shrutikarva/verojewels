90a4bb18c48af0e2cbe30961f1b7ed6fbec4c766
shrutikarva <shruti.karva@gmail.com>
2018-08-05 16:00:21 +0400

    trial

--- verojewels/production.py
+++ verojewels/production.py
@@ -150,6 +150,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
     ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
 
--- verojewels/wsgi.py
+++ verojewels/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "verojewels.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "verojewels.production")
 
 application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
