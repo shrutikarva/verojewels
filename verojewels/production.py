1a3c37dcab57743c0f56b616aaa26245947336b0
shrutikarva <shruti.karva@gmail.com>
2018-08-05 16:14:52 +0400

    trial3

--- verojewels/production.py
+++ verojewels/production.py
@@ -101,8 +101,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
