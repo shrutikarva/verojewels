564f27fffadcff03140640b0cc17bcc191f7d040
shrutikarva <shruti.karva@gmail.com>
2018-08-05 17:35:35 +0400

    trail9

--- verojewels/production.py
+++ verojewels/production.py
@@ -95,11 +95,17 @@ SECURE_FRAME_DENY= True
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
-  }
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vjeweldb',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
+DATABASES['default'] = dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
